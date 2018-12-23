e4caf8524c83acc6d36a532985aa70d7832e7209
LeopoldoIII <nathan.rx@gmail.com>
2018-12-23 10:58:47 -0600

    Inicio del servicio desde Appium

--- src/test/java/test_cases/TestWebBrowser.java
+++ src/test/java/test_cases/TestWebBrowser.java
@@ -1,11 +1,15 @@
 package test_cases;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -16,10 +20,18 @@ public class TestWebBrowser {
 
     public static  void main(String[] args) throws MalformedURLException {
 
+        AppiumDriverLocalService service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder().usingDriverExecutable
+                (new File("C:\\Program Files\\nodejs\\node.exe")).withAppiumJS
+                (new File("C:\\Users\\natha\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js")));
+
+        service.start();
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability(CapabilityType.BROWSER_NAME, "Chrome");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "");
 
 
         driver = new AndroidDriver(new URL("http://localhost:4710/wd/hub"), capabilities);
@@ -30,6 +42,7 @@ public class TestWebBrowser {
         driver.findElement(By.name("q")).sendKeys("Hello Appium");
 
         driver.quit();
+        //service.stop();
 
 
 
